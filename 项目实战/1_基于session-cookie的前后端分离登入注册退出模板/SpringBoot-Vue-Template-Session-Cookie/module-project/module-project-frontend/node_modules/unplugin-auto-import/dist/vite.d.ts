2bc77131e07515dd0cb5c41810bd0cff076c17d5
yexiaopingguo <1056527538@qq.com>
2024-11-25 23:51:02 +0800

    2024.11.25

--- 项目实战/1_基于session-cookie的前后端分离登入注册退出模板/SpringBoot-Vue-Template-Session-Cookie/module-project/module-project-frontend/node_modules/unplugin-auto-import/dist/vite.d.ts
+++
@@ -1,9 +0,0 @@
-import { Options } from './types.js';
-import '@antfu/utils';
-import '@rollup/pluginutils';
-import 'unimport';
-import 'unimport/dist/shared/unimport.d0a971e7';
-
-declare const _default: (options?: Options) => any;
-
-export { _default as default };
